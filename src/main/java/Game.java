284f86a561634365193d919738eea6a1e6843087
avnih <avnih0@gmail.com>
2020-04-15 15:52:08 +0530

    completed step 3 with scanner and Unit testing.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,17 +4,17 @@ public class Game {
     private ScoreBoard scoreBoard;
     private Player playerOne;
     private Player playerTwo;
+    private RuleEngine ruleEngine;
 
-    Game(Player playerOne, Player playerTwo) {
+    Game(Player playerOne, Player playerTwo, RuleEngine ruleEngine) {
         super();
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
         scoreBoard = new ScoreBoard(0,0);
+        this.ruleEngine = ruleEngine;
     }
 
     public ScoreBoard play() {
-        RuleEngine ruleEngine = new RuleEngine();
-
         return ruleEngine.getScore(playerOne.makeMove(),playerTwo.makeMove());
     }
 
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,16 +1,20 @@
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
 
     private Game game;
+    private RuleEngine ruleEngine = new RuleEngine();
 
     @Test
     public void shouldProcessWhenBothMovesAreCooperate() {
         Player cooperatePlayer = new Player(new CooperatePlayerBehavior());
         Player cheatPlayer = new Player(new CooperatePlayerBehavior());
-        game = new Game(cooperatePlayer, cheatPlayer);
+        game = new Game(cooperatePlayer, cheatPlayer, ruleEngine);
         ScoreBoard scores = game.play();
 
         assertEquals(new ScoreBoard(2,2), scores);
@@ -20,7 +24,7 @@ public class GameTest {
     public void shouldGetFinalScoreAfterFiveRoundsForBothCooperate() {
         Player cooperatePlayerOne = new Player(new CooperatePlayerBehavior());
         Player cooperatePlayerTwo = new Player(new CooperatePlayerBehavior());
-        game = new Game(cooperatePlayerOne, cooperatePlayerTwo);
+        game = new Game(cooperatePlayerOne, cooperatePlayerTwo, ruleEngine);
         ScoreBoard finalScore = game.playForNRounds(5);
 
         assertEquals(new ScoreBoard(10,10), finalScore);
@@ -30,9 +34,41 @@ public class GameTest {
     public void shouldGetFinalScoreAfterFiveRoundsForCooperateAndCheat() {
         Player cooperatePlayer = new Player(new CooperatePlayerBehavior());
         Player cheatPlayer = new Player(new CheatPlayerBehavior());
-        game = new Game(cooperatePlayer, cheatPlayer);
+        game = new Game(cooperatePlayer, cheatPlayer, ruleEngine);
         ScoreBoard finalScore = game.playForNRounds(5);
 
         assertEquals(new ScoreBoard(-5,15), finalScore);
+    }
+
+    @Test
+    public void shouldReturnScoreCheatPlayerAndConsolePlayer() {
+        Player cheatPlayer = Mockito.mock(Player.class);
+        Player consolePlayer = Mockito.mock(Player.class);
+        ruleEngine = Mockito.mock(RuleEngine.class);
+
+        Mockito.when(cheatPlayer.makeMove()).thenReturn(MoveType.CHEAT);
+        Mockito.when(consolePlayer.makeMove())
+                .thenReturn(MoveType.CHEAT)
+                .thenReturn(MoveType.COOPERATE);
+        Mockito.when(ruleEngine.getScore(MoveType.CHEAT, MoveType.CHEAT))
+                .thenReturn(new ScoreBoard(0,0));
+        Mockito.when(ruleEngine.getScore(MoveType.CHEAT, MoveType.COOPERATE))
+                .thenReturn(new ScoreBoard(3,-1));
+
+        game = new Game(cheatPlayer, consolePlayer, ruleEngine);
+
+        ScoreBoard finalScore = game.playForNRounds(2);
+
+        assertEquals(new ScoreBoard(3,-1), finalScore);
+    }
+
+    @Test
+    public void shouldReturnFinalScoreCooperatePlayerAndConsolePlayer() {
+        Player cooperatePlayer = new Player(new CooperatePlayerBehavior());
+        Player consolePlayer = new Player(new ConsolePlayerBehavior(new Scanner("CH CO")));
+        game = new Game(cooperatePlayer, consolePlayer, ruleEngine);
+        ScoreBoard finalScore = game.playForNRounds(2);
+
+        assertEquals(new ScoreBoard(1,5), finalScore);
     }
 }
